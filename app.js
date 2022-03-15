d0d5c505328da72e98d3437f6e5dae133e1982a7
Santiago Canovari <santiagocanovari@gmail.com>
2022-03-14 21:41:08 -0300

    :as

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('colors');
 
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo');
 const { inquirerMenu, 
         pausa,
         leerInput
@@ -11,6 +12,13 @@ const main = async () => {
 
     let opt = ''
     const tareas = new Tareas();
+
+    const tareasDB=leerDB();
+
+    if (tareasDB){
+
+    }
+    await pausa();
 
     do {
         //Imprimir el menu
@@ -27,6 +35,8 @@ const main = async () => {
                 console.log(tareas.listadoArr);
             break;
         }
+
+        // guardarDB(tareas.listadoArr);
 
 
         await pausa();
