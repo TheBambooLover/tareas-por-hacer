ccec419782ec2034dc0d8c37cbc457a845d7caa6
Santiago Canovari <santiagocanovari@gmail.com>
2022-03-17 17:05:26 -0300

    Listar tareas upgraded

--- models/tarea.js
+++ models/tarea.js
@@ -3,7 +3,7 @@ const {v4:uuidv4}=require('uuid');
 class Tarea {
     id = '';
     desc = '';
-    completadoEn = null;
+    completadoEn = "null";
     creadoEn = null;
 
     constructor(desc) {
--- models/tareas.js
+++ models/tareas.js
@@ -1,5 +1,7 @@
 const Tarea = require('./tarea');
 
+var colors = require ('colors');
+
 class Tareas{
     _listado = {};  
 
@@ -29,6 +31,28 @@ class Tareas{
     crearTarea(desc = ''){
         const tarea = new Tarea(desc);
         this._listado[tarea.id] = tarea;
+    }
+
+    listadoCompleto(){
+
+        //1: Verde
+        // Completada: Verde
+        // Pendiente ::Rojo
+        //1. Alma :: Completada | Pendiente
+        //2. Poder:: Completada | Pendiente
+        // let completado = this.listadoArr.map(value => value.completadoEn);
+        // return completado
+        let x = 1;
+        this.listadoArr.forEach(tarea => {
+            if (tarea.completadoEn !=null){
+                console.log(colors.green('%s. %s :: Completada'),x,tarea.desc)
+                x++;
+            }else
+            {
+                console.log(colors.red('%s. %s :: Pendiente'),x,tarea.desc)
+                x++;
+            }
+        });
     }
 }
 
